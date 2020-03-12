995631b9f9c602f5459409f85bbde98a10e9020d
Irfan Dwi Prasetyo <keepant@student.uns.ac.id>
2020-03-12 15:35:24 +0700

    added splashscreen

---
+++ lib/screens/spashscreen.dart
@@ -0,0 +1,17 @@
+import 'package:custom_splash/custom_splash.dart';
+import 'package:flutter/material.dart';
+import 'package:sparring/home.dart';
+
+class Splashscreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CustomSplash(
+      imagePath: 'assets/img/soccer-player.png',
+      backGroundColor: Theme.of(context).primaryColor,
+      home: Home(),
+      duration: 2500,
+      type: CustomSplashType.StaticDuration,
+      //animationEffect: 'zoom-in',      
+    );
+  }
+}
