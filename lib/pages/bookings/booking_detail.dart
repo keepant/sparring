147ca8bcf82b0ad15af7f151003351a395968bad
Irfan Dwi Prasetyo <keepant@gmail.com>
2020-04-28 12:59:22 +0700

    added booking details page

--- lib/pages/bookings/booking_detail.dart
+++ lib/pages/bookings/booking_detail.dart
@@ -1,14 +1,195 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:font_awesome_flutter/fa_icon.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:sparring/i18n.dart';
 
 class BookingDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    ScreenUtil.init(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(I18n.of(context).bookingDetailsTitle),
       ),
-      body: Text("data"),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            height: ScreenUtil().setHeight(500),
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: NetworkImage(
+                    'https://ecs7.tokopedia.net/img/cache/700/product-1/2019/3/17/2905360/2905360_bc8d6026-bb5c-4920-bacd-a7b4bb1f9f6b_576_576.jpg'),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 4.0),
+            child: Text(
+              'Lapangan Anugrah Jaya',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16.0, 4.0, 16.0, 4.0),
+            child: Text(
+                "Jl. Gajah Terbang Tinggi Sekall, Karanganyar, Jawa Tengah, Indonesia"),
+          ),
+          Divider(),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16.0, 4.0, 16.0, 4.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  child: Icon(
+                    FontAwesomeIcons.solidCheckCircle,
+                    color: Colors.green,
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(left: 8.0),
+                  child: Text(
+                    'Completed',
+                    style: TextStyle(
+                      color: Colors.green,
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Divider(),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16.0, 4.0, 16.0, 4.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      child: FaIcon(
+                        FontAwesomeIcons.calendarAlt,
+                        size: 14.0,
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(left: 8.0),
+                      child: Text("25 June, 2020"),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      child: FaIcon(
+                        FontAwesomeIcons.clock,
+                        size: 14.0,
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(left: 8.0),
+                      child: Text("13.00 - 15.00"),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: Container(height: 10.0, color: Colors.black12),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 4.0),
+            child: Text(
+              'Payment Details',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16.0, 4.0, 16.0, 4.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  child: Icon(
+                    FontAwesomeIcons.solidCheckCircle,
+                    color: Colors.green,
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(left: 8.0),
+                  child: Text(
+                    'Completed',
+                    style: TextStyle(
+                      color: Colors.green,
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Divider(),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16.0, 4.0, 16.0, 4.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+                  child: Text(
+                    "You pay",
+                    style: TextStyle(fontWeight: FontWeight.w600),
+                  ),
+                ),
+                Container(
+                  child: Text(
+                    "Rp. 50.000",
+                    style: TextStyle(fontWeight: FontWeight.w600),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Divider(),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 4.0),
+            child: Text(
+              'Payment Method',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16.0, 4.0, 16.0, 4.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+                  child: Text(
+                    "Cash On Delivery (COD)",
+                  ),
+                ),
+                Container(
+                  child: Text(
+                    "Irfan Dwi Prasetyo",
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
